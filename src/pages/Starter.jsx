28ddedd705c483883300967326899f4a163f1426
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-03-31 23:17:20 +0700

    to handle localstorage data across in all tab

--- src/pages/Starter.jsx
+++ src/pages/Starter.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { updateUserFromLocalStorage } from "../store/userSlice";
+import { updateRoomsFromLocalStorage } from "../store/chatSlice";
 
 const Starter = () => {
-  return (
-    <div>Starter</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Starter
+  // Listen for changes in the "users" key of localStorage
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const savedUsers = JSON.parse(localStorage.getItem("users"));
+      if (savedUsers) {
+        dispatch(updateUserFromLocalStorage(savedUsers));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [dispatch]);
+
+  // Listen for changes in the "rooms" key of localStorage
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const savedRooms = JSON.parse(localStorage.getItem("rooms"));
+      if (savedRooms) {
+        dispatch(updateRoomsFromLocalStorage(savedRooms));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [dispatch]);
+
+  return <Outlet />;
+};
+
+export default Starter;
