b475b62ab668afb4d8812a6c39be14152ab0e157
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-03-31 22:16:01 +0700

    to store current user info in each tab in redux store

---
+++ src/store/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+      setUserInfo: (state, { payload }) => {
+        state.userInfo = payload;
+      },
+      clearUserInfo: (state, { payload }) => {
+        state.userInfo = null;
+      }
+    }
+})
+
+export const { setUserInfo, clearUserInfo } = authSlice.actions;
+export default authSlice.reducer;
